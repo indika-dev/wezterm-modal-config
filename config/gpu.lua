13e832fc2dc7c6a8b93247d800c3ec04dc8438d5
Stefan Maaßen <s.maassen@verband.creditreform.de>
2024-07-05 10:29:44 +0200

    fix(config): better WebGPU handling

--- config/gpu.lua
+++ config/gpu.lua
@@ -3,7 +3,11 @@ local Config = {}
 
 Config.front_end = "WebGpu"
 Config.webgpu_force_fallback_adapter = false
-Config.webgpu_power_preference = "HighPerformance"
+if os.getenv "USER" == "stefan" then
+  Config.webgpu_power_preference = "HighPerformace"
+else
+  Config.webgpu_power_preference = "LowPower"
+end
 Config.webgpu_preferred_adapter = require("utils.gpu_adapter"):pick_best()
 
 return Config
