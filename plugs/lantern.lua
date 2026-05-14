e23edf31519339dc1c084a52ec54c41a2c7393d1
sravioli <s.fidanza@pm.me>
2026-05-14 09:43:00 +0200

    fix(lantern): preserve tab button formatting

--- plugs/lantern.lua
+++ plugs/lantern.lua
@@ -4,10 +4,15 @@
 
 local lantern = require("wezterm").plugin.require "https://github.com/sravioli/lantern.wz"
 
+local function set_tab_button(cfg, theme)
+  require("utils.color").set_tab_button(cfg, theme)
+end
+
 lantern.setup {
   default_font = require "config.font",
   color = {
     opacity = require("opts").config.color.opacity,
+    set_tab_button = set_tab_button,
   },
 }
 
